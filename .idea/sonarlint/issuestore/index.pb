75dffb223606bb53ab0e40bc8e57ad18e74fcf55
Harshad Riswadkar <riswadkar.harshad@gmail.com>
2022-08-13 20:26:59 +0530

    Created new repo for HackerRankChallenges

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Q
+!src/com/hackerrank/java/Main.java,7\a\7a6ac41f64427919669693541b3f1a38428fe346
+i
+9src/com/hackerrank/java/datatypes/DataTypesChallenge.java,5\3\53df9e2524e3c26612fc2cd2be2b3cf05e0672ab
---
+++ src/com/hackerrank/java/Main.java
@@ -0,0 +1,10 @@
+package com.hackerrank.java;
+
+import com.hackerrank.java.datatypes.DataTypesChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        DataTypesChallenge obj = new DataTypesChallenge();
+        obj.DataTypeSolution(args);
+    }
+}
